2dcc9eaa01f8486e8d3bcc45fa7cad85489330fa
nao20010128nao <nao20010128@gmail.com>
2021-01-31 07:44:32 +0000

    modify for fc2 live

--- api/video.js
+++ api/video.js
@@ -2,8 +2,28 @@ const chromium = require("chrome-aws-lambda");
 
 const wait = (ms) => new Promise((r) => setTimeout(r, ms));
 
+/*
+ *# orz token is JWT token
+ *# header: base64 encoded of {"orz":[cookie.l_ortkn]}
+ *# payload: base64 encoded of {"orz":[cookie.l_ortkn]}
+ *# signature: base64 encoded of {"orz":[cookie.l_ortkn]}
+ *# post to https://live.fc2.com/api/getControlServer.php
+ *#   with: channel_id: video_id
+ *#   with: mode: "mode"
+ *#   with: orz: ***
+ *#   with: channel_version: "33fe385c-5eed-4e15-bb04-a6b5ae438d8e"
+ *#   with: client_version: "2.0.0\n+[1]"
+ *#   with: client_type: "pc"
+ *#   with: client_app: "browser_hls"
+ *#   with: ipv6: ""
+ *# websocket to [url]?control_token=[control_token]
+ *# send {"name":"get_hls_information","arguments":{},"id":1}
+ *# test against .argumtnts.playlists[].url
+ *# done
+ */
+
 module.exports = async (req, res) => {
-  const { id, take } = req.query;
+  const { id } = req.query;
   const { puppeteer } = chromium;
   const browser = await puppeteer.launch({
     args: [...chromium.args, "--autoplay-policy=no-user-gesture-required"],
@@ -11,84 +31,32 @@ module.exports = async (req, res) => {
     env: process.env,
   });
   const page = await browser.newPage();
+  let data = null;
   try {
-    await new Promise((resolve, reject) => {
+    data = await new Promise((resolve, reject) => {
       (async () => {
         page
           .on("console", (message) => console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
           .on("pageerror", ({ message }) => console.log(message))
           .on("response", (response) => console.log(`${response.status()} ${response.url()}`))
           .on("requestfailed", (request) => console.log(`${request.failure().errorText} ${request.url()}`));
-        await page.exposeFunction("reportResponseText", (response) => {
-          res.setHeader("Content-Type", "application/x-mpegurl");
-          res.send(response);
-          resolve();
-        });
         await page.setRequestInterception(true);
         page.on("request", (request) => {
-          if (request.url() == "https://avgle.com/templates/frontend/videojs-contrib-hls.js") {
-            console.log("replacing url 1");
-            request.continue({
-              // replace with patched code
-              url: "https://avgle-m3u8-extractor.vercel.app/videojs-contrib-hls.js",
-            });
-          } else if (request.url().endsWith("?adblock_detected")) {
-            // easy adblocking
+          const url = request.url();
+          if (url.startsWith("wss://") && url.includes("")) {
+            resolve(url);
+            res.send(url);
             request.abort();
-          } else if (request.url().includes("jads.co")) {
-            // easy adblocking
-            request.abort();
-          } else if (request.url().includes("ads.")) {
-            // easy adblocking
-            request.abort();
-          } else if (request.url().includes("recaptcha/api/siteverify")) {
-            // you don't need to honor recaptcha
-            console.log("responding with fake response");
-            request.continue({
-              // replace with fake response
-              body: '{"success":true}',
-            });
-          } else if (request.url().includes("recaptcha")) {
-            // you don't need to honor recaptcha
-            console.log("replacing url 2");
-            request.continue({
-              // replace with fake code
-              url: "https://avgle-m3u8-extractor.vercel.app/recaptcha.js",
-            });
           } else {
             request.continue();
           }
         });
         try {
-          await page.goto(`https://avgle.com/video/${id}/`, {
+          await page.goto(`https://live.fc2.com/${id}/`, {
             timeout: 2000,
           });
         } catch (e) {}
         console.log("loaded");
-        const [button] = await page.$x("//span[@id='player_3x2_close']");
-        if (button) {
-          await button.click();
-        }
-        console.log("clicked");
-        await page.evaluate((_) => {
-          /* eslint no-undef: 0 */
-          closeAd({ screenX: 1, originalEvent: { isTrusted: true } });
-        });
-        console.log("ad closed");
-        await wait(100);
-        await page.evaluate((_) => {
-          /* eslint no-undef: 0 */
-          for (const p of Object.values(videojs.getPlayers())) {
-            p.play();
-          }
-        });
-        console.log("video played");
-        if (take == "yes" || take == "take") {
-          // send screenshot for debugging
-          const img = await page.screenshot({ type: "png" });
-          res.setHeader("Content-Type", "image/png");
-          res.send(img);
-        }
       })().catch(reject);
     });
   } finally {
--- test.js
+++
@@ -1,4 +0,0 @@
-const id = process.argv[2];
-const r = require("./api/video.js");
-
-r({ query: { id, take: "yes" } }, { send: console.log, end: console.log });
